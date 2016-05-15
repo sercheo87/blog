3537575c974e05765fdbf8b19a0dc8f684308f78
sercheo87 <chancaysergio@gmail.com>
2016-05-15 10:38:39 -0500

    Categorias dinamicas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515151916) do
+ActiveRecord::Schema.define(version: 20160515152015) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20160515151916) do
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
